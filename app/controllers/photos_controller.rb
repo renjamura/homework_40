7cada6402a92724b10bc6d733cf0739104796857
Murat Djumaliev <renja.dizz@gmail.com>
2017-10-05 03:51:07 +0600

    Finish Photo model
    Closed all functionality, added index for each user

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,18 +1,16 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  before_action :photo_owner, only: [:edit, :update, :destroy]
 
   # GET /photos
   # GET /photos.json
   def index
-    @photos = Photo.all
+    @photos = Photo.all.order(created_at: :desc)
   end
-
   # GET /photos/1
   # GET /photos/1.json
   def show
-    @photo = current_user.photos.find(params[:id])
   end
-
   # GET /photos/new
   def new
     @photo = current_user.photos.build
@@ -21,60 +19,54 @@ class PhotosController < ApplicationController
       format.xml  { render :xml => @photo }
     end
   end
-
   # GET /photos/1/edit
   def edit
-    @photo = current_user.photos.find(params[:id])
   end
-
   # POST /photos
   # POST /photos.json
   def create
-
     @photo = current_user.photos.create!(photo_params)
-
-    
-      if @photo.save
-       redirect_to user_photo_path(@photo)
-      else
-       render :new 
-       
-      end
-    
+    if @photo.save
+     redirect_to user_photo_path(@photo)
+    else
+      render :new 
+    end
   end
-
   # PATCH/PUT /photos/1
   # PATCH/PUT /photos/1.json
   def update
-
-      respond_to do |format|
-      if @photo.update(photo_params)
-        format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @photo }
-      else
-        format.html { render :edit }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
-      end
+    if @photo.update(photo_params)
+      redirect_to user_photos_path
+    else
+      render :edit
     end
   end
-
   # DELETE /photos/1
   # DELETE /photos/1.json
   def destroy
-    @photo = current_user.photos.find(params[:id])
     @photo.destroy
     respond_to do |format|
       format.html { redirect_to user_photos_url, notice: 'Photo was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
-
+  def personal
+    @photo = Photo.find(params[:photo_id])
+    @user = @photo.user
+    @photos = @user.photos.all
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_photo
-      
+      @photo = Photo.find(params[:id])
     end
-
+    def photo_owner
+      @photo = Photo.find(params[:id])
+      if (@photo.user_id != current_user.id)
+        flash[:notice] = 'Access denied as you are not owner of this Photo'
+        redirect_to root_path
+      end
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
       params.require(:photo).permit(:title, :body, :image, :user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   devise_for :users, :paths => 'users'
 
   resource :user do
-    resources :photos
+    resources :photos do
+   	  get 'personal'
+    end
   end
-
+  
   root 'photos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    
