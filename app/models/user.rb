1c28c7daa2057dc0d19f34edc4494f315f25141b
Murat Djumaliev <renja.dizz@gmail.com>
2017-10-04 23:29:37 +0600

    Add User Model
    Add User Model Devise, sign in with username

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  attr_accessor :username
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
